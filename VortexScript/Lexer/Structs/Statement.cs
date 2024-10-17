b38800ecc53dd65c2510ca809e28bfca85179327
Alexandr Vituško <alanvitusko@gmail.com>
2024-10-17 19:31:23 +0200

    working on lexer

--- VortexScript/Lexer/Structs/Statement.cs
+++ VortexScript/Lexer/Structs/Statement.cs
@@ -9,7 +9,7 @@ public struct Statement
 
 public class StatementType(string StartsWith, bool StartsNewScope, ScopeTypeEnum scopeType = ScopeTypeEnum.topLevel, bool endsScope = false)
 {
-    public string StartsWith { get; private set; } = StartsWith;
+    public string StartsWith { get; private set; } = StartsWith; //if empty, this statement cannot be determined simply by keywords (e.g. declare function, assigment)
     public bool StartsNewScope { get; private set; } = StartsNewScope;
     public ScopeTypeEnum ScopeType { get; private set; } = scopeType;
     public bool EndsScope { get; private set; } = endsScope;
@@ -17,9 +17,9 @@ public class StatementType(string StartsWith, bool StartsNewScope, ScopeTypeEnum
     public List<TokenGroup?> statementStruct = new();
 
     bool groupOpen=false;
-    public StatementType StartGroup(bool and = false){
+    public StatementType StartGroup(GroupRule multiple = GroupRule.GroupAny,bool fixed_ = false, bool mandatory = true){
         groupOpen = true;
-        statementStruct.Add(new TokenGroup([],and));
+        statementStruct.Add(new TokenGroup([],multiple,fixed_,mandatory));
         return this;
     }
     public StatementType EndGroup(){
@@ -27,36 +27,33 @@ public class StatementType(string StartsWith, bool StartsNewScope, ScopeTypeEnum
         statementStruct.Add(null);
         return this;
     }
-
-    public StatementType Expect(TokenType type, bool mandatory = true)
-    {
-        Add(new(type,""));
-        return this;
-    }
-    public StatementType Expect(string syntax, bool mandatory = true)
-    {
-        Add(new(TokenType.Syntax,syntax,mandatory));
-        return this;
-    }
-    public StatementType ExpectRightAfter(TokenType type, bool mandatory = true)
-    {
-        Add(new(type,"",mandatory,true));
-        return this;
-    }
-    public StatementType ExpectRightAfter(string syntax, bool mandatory = true)
-    {
-        Add(new(TokenType.Syntax,syntax,mandatory));
-        return this;
-    }
     
-    void Add(Token token,bool fixed_ = false,bool mandatory = true){
+    public StatementType Expect(string val,bool mandatory = true){
+        Add(new(TokenType.Syntax,val),GroupRule.GroupAny,false,mandatory);
+        return this;
+    }
+    public StatementType ExpectAfter(string val,bool mandatory = true){
+        Add(new(TokenType.Syntax,val),GroupRule.GroupAny,true,mandatory);
+        return this;
+    }
+    public StatementType Expect(TokenType type,bool mandatory = true){
+        Add(new(type,""),GroupRule.GroupAny,false,mandatory);
+        return this;
+    }
+    public StatementType ExpectAfter(TokenType type,bool mandatory = true){
+        Add(new(type,""),GroupRule.GroupAny,true,mandatory);
+        return this;
+    }
+
+    void Add(Token token,GroupRule multiple = GroupRule.GroupAny,bool fixed_ = false, bool mandatory = true){
         if(groupOpen){
             statementStruct.FindLast(x=>x!=null)!.tokens.Add(token);
         }
         else{
-            statementStruct.Add(new TokenGroup(new List<Token>(){token}));
+            statementStruct.Add(new TokenGroup(new List<Token>(){token},multiple,fixed_,mandatory));
         }
     }
+    
 
 
     public static StatementType DeclareStatement;
@@ -65,7 +62,15 @@ public class StatementType(string StartsWith, bool StartsNewScope, ScopeTypeEnum
     {
 
         DeclareStatement = new("$", false);
-        DeclareStatement.Expect("$").StartGroup().ExpectRightAfter("").ExpectRightAfter();
+        DeclareStatement.Expect("$")
+                        .StartGroup(GroupRule.GroupAny,true,false)
+                        .Expect("?")
+                        .Expect("!")
+                        .EndGroup()
+                        .ExpectAfter(TokenType.DecleareIdentifier)
+                        .StartGroup(GroupRule.GroupAll,false,false)
+                        .Expect("=")
+                        .Expect(TokenType.Expression);
     }
 
 
@@ -74,14 +79,14 @@ public class StatementType(string StartsWith, bool StartsNewScope, ScopeTypeEnum
 public class TokenGroup
 {
     public List<Token> tokens = new();
-    public bool and = false;
-    public bool fixed_ = true;
-    
-    public bool mandatory = false;
+    public GroupRule groupType = GroupRule.GroupAny; 
+    public bool fixed_ = false; //expect right after. No white space is allowed when true
+    public bool mandatory = true; //if true- the group may be ommited
 
-    public TokenGroup(List<Token> tokens,bool and = false,bool mandatory = true){
+    public TokenGroup(List<Token> tokens,GroupRule groupType = GroupRule.GroupAny,bool fixed_ = false, bool mandatory = true){
         this.tokens = tokens;
-        this.and = and;
+        this.groupType = groupType;
+        this.fixed_ = fixed_;
         this.mandatory = mandatory;
     }
 
@@ -103,10 +108,16 @@ public class Token
 public enum TokenType
 {
     Identifier,
+    DecleareIdentifier,
     Expression,
     StartScope,
     EndScope,
     Syntax,
     Args,
     Group
+}
+public enum GroupRule{
+    GroupAny = 0,
+    GroupAll = 1,
+    GroupExactlyOne = 2
 }
